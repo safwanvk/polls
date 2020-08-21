3166d83fdd56989b46869be31800e63282c2bdb8
safwanvk <safwanvalakundil@gmail.com>
2020-08-21 14:43:17 +0530

    :tada: Custamize admin form

--- polls/admin.py
+++ polls/admin.py
@@ -3,18 +3,24 @@ from .models import Question, Choice
 
 # Register your models here.
 
-class QuestionAdmin(admin.ModelAdmin):
-    list_display = [
-        'question_text',
-        'pub_date'
-    ]
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
-class ChoiceAdmin(admin.ModelAdmin):
-    list_display = [
-        'question',
-        'choice_text',
-        'votes'
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+    inlines = [ChoiceInline]
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice, ChoiceAdmin)
+
+admin.site.register(Choice)
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
